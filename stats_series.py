84000d9b54f14c2a99335a76f5fb94618da9c9fc
Wojciech Korbel <251169@edu.p.lodz.pl>
2025-07-21 12:43:16 +0200

    Funkcja obliczajaca najdluzsza serie bez straty gola

--- stats_series.py
+++ stats_series.py
@@ -212,6 +212,29 @@ def get_longest_streak_of_losing_a_goal(team_goals):
     return result
 
 
+# najdłuższa seria bez straty gola
+def get_longest_streak_without_losing_a_goal(team_goals):
+    matches = len(team_goals)
+    if matches == 0:
+        return 0
+    if matches == 1 and team_goals[0]['opponentGoals'] == 0:
+        return 1
+
+    result = 0
+    curr_len = 0
+    for el in team_goals:
+        if el['opponentGoals'] == 0:
+            curr_len += 1
+        else:
+            if curr_len > result:
+                result = curr_len
+            curr_len = 0
+
+    if curr_len > result:
+        result = curr_len
+    return result
+
+
 # TODO do usuniecia pozniej
 if __name__ == '__main__':
     matches_data = load_data("data_csv/ekstraklasa2425")
@@ -228,3 +251,4 @@ if __name__ == '__main__':
     print(legia_goals)
     display_teams_goals(legia_goals)
     print(get_longest_streak_of_losing_a_goal(legia_goals))
+    print(get_longest_streak_without_losing_a_goal(legia_goals))
